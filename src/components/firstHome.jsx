028320f62d0003b04d05e874811cfb7462b61bae
narendranaragani <narendranaragani7@gmail.com>
2025-09-13 21:09:42 +0530

    Update firstHome.jsx

--- src/components/firstHome.jsx
+++ src/components/firstHome.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 const FirstHome = () => {
   const navigate = useNavigate();
-
   return (
     <div className="relative h-screen w-full bg-black overflow-hidden">
       <video
