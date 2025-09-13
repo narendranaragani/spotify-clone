83424b76278d67706d722fe11b19570ae52c318a
narendranaragani <narendranaragani7@gmail.com>
2025-09-13 18:08:13 +0530

    Update firstHome.jsx

--- src/components/firstHome.jsx
+++ src/components/firstHome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useNavigate } from "react-router";
-import {Link} from "react"
+import { Link } from 'react-router-dom';
 
 const FirstHome = () => {
   // const navigate = useNavigate();
