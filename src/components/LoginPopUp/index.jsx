cd66d70d33a748ae69ab012e5b84743130261953
narendranaragani <narendranaragani7@gmail.com>
2025-09-13 18:42:00 +0530

    Update index.jsx

--- src/components/LoginPopUp/index.jsx
+++ src/components/LoginPopUp/index.jsx
@@ -30,7 +30,7 @@ const LoginForm = () => {
 
   const token = Cookies.get("jwt_token");
   if (token !== undefined) {
-    return <Navigate to="/" replace />;
+    return <Navigate to="/home" replace />;
   }
 
   const onSubmitEvent = async (event) => {
